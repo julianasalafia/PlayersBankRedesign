e79b9524b1e5343db6ff3a1c3cf605a3e1ba816a
julianasalafia <juliana.salafia1@gmail.com>
2023-07-30 18:45:42 -0300

    [PB]: add components to login page

--- lib/main.dart
+++ lib/main.dart
@@ -15,67 +15,110 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
         primaryColor: AppColors.white,
         scaffoldBackgroundColor: AppColors.backgroundColor,
       ),
       home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      height: 45.0,
-                      color: AppColors.orange,
-                      child: TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'ENTRAR',
-                            style: TextStyle(
-                              fontFamily: 'Barracuda',
-                              fontSize: 16.0,
-                              color: AppColors.backgroundColor,
-                            ),
-                          )),
-                    ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(15, 45, 15, 15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 50,
+                  child: Image.asset(
+                    'assets/images/pb_logo.png',
+                    fit: BoxFit.scaleDown,
                   ),
-                ],
-              ),
-              SizedBox(height: 10.0),
-              Row(
-                children: [
-                  Expanded(
-                      child: Container(
-                    height: 45.0,
-                    child: TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          'QUERO MINHA CONTA',
-                          style: TextStyle(
-                            fontFamily: 'Barracuda',
-                            fontSize: 16.0,
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  'O BANCO QUE',
+                  style: TextStyle(
+                    fontFamily: 'Barracuda',
+                    fontSize: 35.0,
+                    color: AppColors.white,
+                  ),
+                ),
+                Text(
+                  'JOGA JUNTO',
+                  style: TextStyle(
+                    fontFamily: 'Barracuda',
+                    fontSize: 35.0,
+                    color: AppColors.orange,
+                  ),
+                ),
+                Container(
+                  height: screenHeight * 0.5,
+                  width: screenWidth * 0.5,
+                  child: Image.asset(
+                    'assets/images/controle.png',
+                    fit: BoxFit.scaleDown,
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            height: 45.0,
                             color: AppColors.orange,
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Text(
+                                  'ENTRAR',
+                                  style: TextStyle(
+                                    fontFamily: 'Barracuda',
+                                    fontSize: 16.0,
+                                    color: AppColors.backgroundColor,
+                                  ),
+                                )),
                           ),
                         ),
-                        style: ButtonStyle(
-                          shape: MaterialStateProperty.all(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(0),
-                            ),
-                          ),
-                          side: MaterialStateProperty.all(
-                            BorderSide(width: 2, color: AppColors.orange),
-                          ),
+                      ],
+                    ),
+                    SizedBox(height: 10.0),
+                    Row(
+                      children: [
+                        Expanded(
+                            child: Container(
+                          height: 45.0,
+                          child: TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                'QUERO MINHA CONTA',
+                                style: TextStyle(
+                                  fontFamily: 'Barracuda',
+                                  fontSize: 16.0,
+                                  color: AppColors.orange,
+                                ),
+                              ),
+                              style: ButtonStyle(
+                                shape: MaterialStateProperty.all(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(0),
+                                  ),
+                                ),
+                                side: MaterialStateProperty.all(
+                                  BorderSide(width: 2, color: AppColors.orange),
+                                ),
+                              )),
                         )),
-                  )),
-                ],
-              ),
-            ],
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
