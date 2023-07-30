4654cb6f9d694d0dd440dbc90bfb681271463b66
julianasalafia <juliana.salafia1@gmail.com>
2023-07-30 16:56:52 -0300

    [PB]: add assets, fonts and shared folders

---
+++ lib/shared/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color orange = const Color(0xffff3c00);
+  static Color backgroundColor = const Color(0xff191921);
+  static Color white = const Color(0xffffffff);
+  static Color black = const Color(0xff000000);
+}
