3951e39aaefd77ce2eebca69d39b8815a015d268
julianasalafia <juliana.salafia1@gmail.com>
2023-08-01 15:16:38 -0300

    [LAYOUT]: add name, birth, document fields, country and state - need to update its style thought.

--- lib/login/sign_up/user_data_page.dart
+++ lib/login/sign_up/user_data_page.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:players_bank_redesign/shared/app_colors.dart';
 import 'package:players_bank_redesign/shared/constants.dart';
 import 'package:players_bank_redesign/widgets/main_button.dart';
 import 'package:players_bank_redesign/widgets/text_field_widget.dart';
 import '../../widgets/go_back_button.dart';
 import 'nickname_page.dart';
+import 'package:country_state_picker/country_state_picker.dart';
 
 const continueButton = 'CONTINUAR';
 const documentTitle = 'CADASTRO';
 
-class UserDataPage extends StatelessWidget {
+class UserDataPage extends StatefulWidget {
   const UserDataPage({super.key});
+
+  @override
+  State<UserDataPage> createState() => _UserDataPageState();
+}
+
+class _UserDataPageState extends State<UserDataPage> {
+  String? state;
+  String? country;
 
   @override
   Widget build(BuildContext context) {
     Widget nickNamePage = const NickNamePage();
-
+    bool showLabel = false;
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.fromLTRB(15, 45, 15, 15),
@@ -42,6 +52,48 @@ class UserDataPage extends StatelessWidget {
                 const TextFieldWidget(hintText: 'Meu RG'),
                 const SizedBox(height: sizedBoxHeight),
                 const TextFieldWidget(hintText: 'Órgão Emissor do RG'),
+                const SizedBox(height: sizedBoxHeight),
+                CountryStatePicker(
+                  stateLabel: Visibility(
+                    visible: showLabel,
+                    child: Label(
+                      title: '',
+                    ),
+                  ),
+                  countryLabel: Visibility(
+                    visible: showLabel,
+                    child: Label(
+                      title: '',
+                    ),
+                  ),
+                  dropdownColor: AppColors.grey,
+                  inputDecoration: const InputDecoration(
+                    hintStyle: kEditTextStyle,
+                    contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.zero,
+                      borderSide: BorderSide(color: AppColors.orange, width: 2),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.zero,
+                      borderSide: BorderSide(color: AppColors.orange, width: 2),
+                    ),
+                    isCollapsed: true,
+                  ),
+                  itemTextStyle: kEditTextStyle,
+                  hintTextStyle: kEditTextStyle,
+                  onCountryChanged: (ct) => setState(() {
+                    country = ct;
+                    state = null;
+                  }),
+                  onStateChanged: (st) => setState(() {
+                    state = st;
+                  }),
+                ),
+                const SizedBox(height: sizedBoxHeight),
               ],
             ),
             Column(
